597a21fca527410a35422ec074475777df1a1b93
ppolett <pardi.polett@gmail.com>
2022-01-01 12:18:31 +0100

    Start

---
+++ Szepsegszalon_WPF_PPP/Szepsegszalon_WPF_PPP/Model/ViewModel2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szepsegszalon_WPF_PPP.Model
+{
+    class ViewModel2
+    {
+    }
+}
